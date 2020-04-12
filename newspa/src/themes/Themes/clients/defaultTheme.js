4f44acf69942f51487e3fcfb6b1162888aedf6cb
Raphael Peruzzolo <raphaelperuzzolo@gmail.com>
2020-04-12 18:28:25 -0300

    Import primary font and create dark theme palette

--- newspa/src/themes/Themes/clients/defaultTheme.js
+++ newspa/src/themes/Themes/clients/defaultTheme.js
@@ -1,6 +1,6 @@
 const genericDesign = {
   fonts: {
-    primary: 'Helvetica Neue, Helvetica, Roboto, sans-serif',
+    primary: 'Montserrat, sans-serif',
     pre: 'Consolas, Liberation Mono, Menlo, Courier, monospace',
     quote: 'Georgia, serif',
   },
@@ -12,18 +12,18 @@ const genericDesign = {
 const darkLight = {
   dark: {
     colors: {
-      primary: ['#1D223B', '#554971', '#8C6ff1', '#8AC6D0', '#B8F3FF'],
-      secundary: ['#FABB61', '#FC9777', '#7A606F', '#5BA7D9','#56E39F'],
+      primary: ['#1D223B', '#554971', '#8C6ff1', '#6b6ced', '#B8F3FF'],
+      secundary: ['#FABB61', '#FC9777', '#7A606F', '#5BA7D9', '#56E39F'],
       background: ['#1e243d', '#16192f', '#291e38'],
-      text: '#F2F2F2',
+      text: ['#E1E1E7', '#8b95b5'],
     },
   },
   light: {
     colors: {
       primary: '#7F7F7F',
       secundary: '#CCCCCC',
-      background: ['#CCCCCC', '#EEEEEE', '#BDA1EC'],
-      text: '#33333',
+      background: ['#DDDDDD', '#E1E1E7', '#C4A9F2'],
+      text: '#1D223B',
     },
   },
 }
