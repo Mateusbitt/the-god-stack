32a5553b71293ce708216da67be19adcc1a684f9
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-04-12 18:47:42 +0100

    Adding console debug to storybook

--- newspa/.storybook/preview.js
+++ newspa/.storybook/preview.js
@@ -1,4 +1,10 @@
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
- 
+import '@storybook/addon-console';
+import { setConsoleOptions } from '@storybook/addon-console';
+
+setConsoleOptions({
+panelExclude: [],
+});
+
 addDecorator(withInfo); 
