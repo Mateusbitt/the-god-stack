1403a2bb0d82331a4f44a1d7f80d6bb987d0eab4
Raphael Peruzzolo <raphaelperuzzolo@gmail.com>
2020-05-06 22:03:42 -0300

    Creating Img atom

---
+++ newspa/src/components/atoms/Img/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const Img = ({ ...props }) => {
+  return (
+    <img {...props} />
+  )
+}
+
+// Img.propTypes = {
+//   children: PropTypes.any,
+// }
+
+export default Img
